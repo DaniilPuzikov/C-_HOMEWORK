3723883a1223a44950c917e6f6869dc3210067ad
Daniil <puzikovdaniil1@outlook.com>
2023-03-17 19:39:09 +0300

    Homework2

---
+++ C#_homework2/TASK2/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = a;
+int size = 0;
+while(b>0)
+{
+    b = b/10;
+    size++;
+}
+b = a;
+int[] array = new int[size];
+int index = 0;
+while(index<size)
+{
+    array[index] = b%10;
+    b=b/10;
+    index++;
+}
+if(size-3<0)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+Console.WriteLine($"Третья цифра в числе - это {array[size-3]}");
---
+++ C#_homework2/TASK4/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 11 - HARD На входе от пользователя целое число любой разрядности, надо удалить вторую цифру слева направо этого числа.
+
+
+
+
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = a;
+int size = 0;
+while(b>0)
+{
+    b = b/10;
+    size++;
+}
+b = a;
+int[] array = new int[size];
+int index = 0;
+while(index<size)
+{
+    array[index] = b%10;
+    b=b/10;
+    index++;
+}
+index = 0;
+if(size-2<0)
+{
+    Console.WriteLine("Второй цифры нет");
+}
+else
+while(index<size)
+{
+   if(array[size-1-index]!=array[size-2])
+   {
+   Console.Write(array[size-1-index]) ;
+   }
+   index++;
+}
