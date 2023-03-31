bf9699cb2ef3e30e458d5c9ffff7d8b2578c1455
Daniil <puzikovdaniil1@outlook.com>
2023-03-31 17:10:24 +0300

    homework6

---
+++ C#_homework6/TASK1/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 5
+// 0, 7, 8, -2, -2 -> 2
+
+// 5
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите количество вводимых чисел");
+int a = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[a];
+FillArray(array);
+Console.WriteLine($"Количество чисел больше 0 равно {Count(array)}");
+
+
+void FillArray(int[] array)
+{
+    Console.WriteLine("Поочерёдно введите числа");
+    for(int i = 0;i<array.Length;i++)
+    {
+        array[i]=Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+int Count(int[] array)
+{
+    int sum = 0;
+    for(int i = 0;i<array.Length;i++)
+    {
+        if(array[i]>0)
+            sum++;
+    }
+    return sum;
+}
---
+++ C#_homework6/TASK4/Program.cs
@@ -0,0 +1,67 @@
+﻿// задача 40 - HARD необязательная. На вход программы подаются три целых положительных числа. 
+// Определить , является ли это сторонами треугольника. Если да, 
+// то вывести всю информацию по нему - площадь, периметр, значения углов треугольника в градусах,
+// является ли он прямоугольным, равнобедренным, равносторонним.
+
+Console.WriteLine("Введите размер первой стороны");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите размер второй стороны");
+double b = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите размер третьей стороны");
+double c = Convert.ToDouble(Console.ReadLine());
+double[] corners = new double[3];
+if(IsReal(a,b,c)==true)
+{
+    Console.WriteLine($"S = {Square(a,b,c)}");
+    Console.WriteLine($"P = {Perimetr(a,b,c)}");
+    Console.WriteLine(Types1(a,b,c));
+    Console.WriteLine(Types2(a,b,c));
+}
+else
+Console.WriteLine("Такого треугольника не существует");
+
+bool IsReal(double a, double b, double c)
+{
+    if(a<b+c && b<c+a && c<b+a)
+        return true;
+    else
+    return false;
+}
+
+double Square (double a, double b, double c)
+{
+    double s = 0;
+    double p = (a+b+c)/2;
+    s = Math.Sqrt(p*(p-a)*(p-b)*(p-c));
+    return s;
+}
+
+double Perimetr(double a, double b, double c)
+{
+    return a+b+c;
+}
+
+string Types1(double a, double b, double c)
+{
+    string t = String.Empty;
+    if(a*a==b*b+c*c || b*b==a*a+c*c || c*c==a*a+b*b)
+        t = "Прямоугольный";
+    else t = "Не прямоугольный";
+    return t;
+}
+
+string Types2(double a, double b, double c)
+{
+    string t = String.Empty;
+    if(a==b && a==c && b==c)
+        t = "Равносторонний";
+    else 
+    if((a==b && a!=c && b!=c)||(a==c && a!=b && b!=c)||(b==c &&  a!=b && a!=c))
+        t = "Равноберенный";
+    else
+    t = "Разносторонний";
+    return t;
+}
+
+
+
