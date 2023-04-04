eb3dc833f0c9c97b34390c050f6a549ff73aefbb
Daniil <puzikovdaniil1@outlook.com>
2023-04-04 19:43:14 +0300

    homework7

---
+++ C#_homework7/TASK2/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает значение элемента в двумерном массиве, и возвращает позицию этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+
+// 1 4 7 2
+
+// 5 9 2 3
+
+// 8 4 2 4
+
+Console.WriteLine("Введите количество строк");
+int i = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int j = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите искомое число");
+int f = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[i,j];
+int[] index = {-1,-1};
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+Find(array,f,index);
+if(index[0]>-1 && index[1]>-1)
+{
+    Console.WriteLine($"Число {f} находится в массиве под индексами [{index[0]},{index[1]}]");
+}
+else
+Console.WriteLine($"Число {f} в массиве отсутствует");
+
+
+
+void FillArray(int[,] array)
+{
+    for(int i = 0; i<array.GetLength(0);i++)
+        for(int j = 0; j<array.GetLength(1);j++)
+            array[i,j] = new Random().Next(0,11);
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i=0;i<array.GetLength(0);i++)
+        {
+        for (int j=0;j<array.GetLength(1);j++)
+            Console.Write($"{array[i,j],3} \t");
+        Console.WriteLine();
+        }
+}
+
+void Find(int[,] array, int f, int[] index)
+{
+    for(int i = 0; i<array.GetLength(0);i++)
+        for(int j = 0; j<array.GetLength(1);j++)
+        {
+            if(array[i,j] == f)
+            {
+                index[0] = i;
+                index[1] = j;
+                break;
+            } 
+        }        
+}
+
---
+++ C#_homework7/TASK4/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача HARD SORT необязательная. Считается за три обязательных
+// Задайте двумерный массив из целых чисел. Количество строк и столбцов задается с клавиатуры. Отсортировать элементы по возрастанию слева направо и сверху вниз.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 10 3
+
+// После сортировки
+// 1 2 3 4
+// 5 7 9 10
+
+Console.WriteLine("Введите количество строк");
+int i = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int j = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[i,j];
+int[] oneline = new int[i*j]; 
+FillArray(array);
+PrintArray(array);
+ToOneLineArray(array,oneline);
+PrintArrayOneLine(oneline);
+
+
+
+void FillArray(int[,] array)
+{
+    for(int i = 0; i<array.GetLength(0);i++)
+        for(int j = 0; j<array.GetLength(1);j++)
+            array[i,j] = new Random().Next(0,11);
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i=0;i<array.GetLength(0);i++)
+        {
+        for (int j=0;j<array.GetLength(1);j++)
+            Console.Write($"{array[i,j],3} \t");
+        Console.WriteLine();
+        }
+}
+
+void ToOneLineArray(int[,] array1, int[] array2)
+{
+    for(int i = 0; i<array1.GetLength(0);i++)   
+        for(int j = 0; j<array1.GetLength(1);j++)
+            for(int x = 0; x<array2.Length;x++)
+                array2[x] = array1[i,j];
+}
+
+void PrintArrayOneLine(int[] array)
+{
+    for(int x = 0; x<array.Length;x++)
+        Console.Write($"{array[x]} ");
+    Console.WriteLine();
+}
